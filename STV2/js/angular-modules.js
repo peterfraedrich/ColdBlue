f520a5b81c11f596a4d4c49a9d4ef7c8d3b39623
dorkware <dorkware@git.code.sf.net>
2014-07-02 00:17:30 -0400

    almost final commit

--- STV2/js/angular-modules.js
+++ STV2/js/angular-modules.js
@@ -243,18 +243,6 @@ app.controller('servList', function($scope, $http, $templateCache) {
 
     };
 
-    ///////// HIDE BUTTONS /////////////////
-
-    $scope.iphide = function() {
-        $scope.iphide = ($scope.iphide) ? false : true;
-        console.log($scope.iphide);
-        return $scope.iphide;   
-
-    };
-
-
-    //////////////////////////////////////////
-
     $scope.add = function () {
 
         var ipData = {
