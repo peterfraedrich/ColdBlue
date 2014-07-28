71cfb74fdb500dbccc797935870d272f5dce4fd8
Peter Fraedrich <pfraedrich@networkmgmt.com>
2014-07-28 14:15:13 -0400

    holy crap it works

--- STV-INSTALL/server_install.sh
+++ STV-INSTALL/server_install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# make user superuser
-sudo su
-
 # go to root dir
 cd /
 
@@ -15,12 +12,12 @@ git clone https://github.com/peterfraedrich/ColdBlue.git
 # install node.js
 rpm --import https://fedoraproject.org/static/0608B895.txt
 rpm -Uvh http://download-i2.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
-yum install nodejs npm --enablerepo=epel
+yum install -y nodejs npm --enablerepo=epel
 
 # install npm stuff
 cd /ColdBlue/STV2/
-npm install inherits
-npm install nodemon # for dev only, unless we want to use it for prod
+npm install inherits -g
+npm install nodemon -g # for dev only, unless we want to use it for prod
 npm install
 
 # add firewall rules
